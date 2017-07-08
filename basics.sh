b4d69dd668730d620e4c1c733b2f65ece55a2ef6
eiki takeuchi <eiki.e.t.d@gmail.com>
2017-07-08 15:00:18 +0900

    Add add-box() & show-box-list()

--- basics.sh
+++ basics.sh
@@ -19,3 +19,18 @@ function show-box-help()
 {
   vagrant box -h
 }
+
+# Description:
+#  Add box
+function add-box()
+{
+  box_url=$1
+  vagrant box add ${box_url}
+}
+
+# Description:
+#   Show box list.
+function show-box-list()
+{
+  vagrant box list
+}
