38ec4c88cd6e260da38e4d9de3300e159a8684e8
eiki takeuchi <eiki.e.t.d@gmail.com>
2017-07-08 14:52:03 +0900

    Add box subcommand help

--- basics.sh
+++ basics.sh
@@ -1,10 +1,21 @@
 
+# Description:
+#   Show vagrant version.
 function check-version()
 {
   vagrant -v
 }
 
+# Description:
+#   Show help.
 function show-help()
 {
   vagrant -h
+}
+
+# Description:
+#   Show subcommand(box) help.
+function show-box-help()
+{
+  vagrant box -h
 }
